e564761fc0f55b764728c0646f42d39f08242bb3
KAsare1 <asarek977@gmail.com>
2024-05-24 18:30:21 +0000

    minor fix

--- DeliveryBackend/authentication/views.py
+++ DeliveryBackend/authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from requests import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny
 from .models import Rider, User
 from .serializers import RiderSerializer, UserSerializer
@@ -14,7 +14,7 @@ class UserSignupView(generics.CreateAPIView):
     
 
 
-class RiderSignupView(generics.CreateAPIView):
+class RiderSignupView(viewsets.ModelViewSet):
     queryset = Rider.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RiderSerializer
