784d69241eaae142bb7172ddd2f3fdf25128c5e9
KAsare1 <asarek977@gmail.com>
2024-05-25 13:21:00 +0000

    minor bug fixes

--- DeliveryBackend/DeliveryBackend/settings/base.py
+++ DeliveryBackend/DeliveryBackend/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,12 +88,15 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES=  {
+    'default': dj_database_url.parse("postgres://swift_db_dev_user:8ZNiofKLOJcg7XkFELSqXwN1slst7eRQ@dpg-cp8u6jf109ks73a0p2dg-a.frankfurt-postgres.render.com/swift_db_dev")}
 
 
 # Password validation
