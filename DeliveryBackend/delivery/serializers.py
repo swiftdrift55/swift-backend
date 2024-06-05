66924ead60a9975c7921cbd96c63898f58325812
KAsare1 <asarek977@gmail.com>
2024-06-05 17:31:11 +0000

    request refactor

--- DeliveryBackend/delivery/serializers.py
+++ DeliveryBackend/delivery/serializers.py
@@ -5,4 +5,4 @@ from .models import DeliveryRequest
 class DeliveryRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = DeliveryRequest
-        fields = '__all__'
+        fields = ('customer', 'item_name', 'pickup_address', 'sender_location', 'receiver_name', 'receiver_mobile_number', 'payment_status', 'payment_value', 'status')
